19670d81ca6ca27953cbf3e6cb3a39e8a43757de
Tung Thanh Le <ttungl@users.noreply.github.com>
2016-11-21 19:32:21 -0600

    Interfaces: a collection of abstract methods

---
+++ day 19.java
@@ -0,0 +1,36 @@
+// https://www.hackerrank.com/challenges/30-interfaces
+// day 19: Interface
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic{
+    
+    public int divisorSum(int n){
+        int sum = 1;
+        int max = sum;
+        
+        for(int i=2; i<= n; i++){
+            if(n%i==0){
+                sum +=i;
+            }
+        }
+        return sum;
+    }
+}
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
