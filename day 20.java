3f15b6588a403d896767d029fadcb16e8924d57f
Tung Thanh Le <ttungl@users.noreply.github.com>
2017-06-01 14:29:29 -0500

    bubblesort

--- day 20.java
+++ day 20.java
@@ -22,7 +22,8 @@ public class Solution {
         for(int i=0; i<n; i++){
             // for(int j=0; j<(n-1); j++){
             for(int j=1; j<(n-i); j++){    
-                if (a[j]>a[j+1]){
+                // if (a[j]>a[j+1]){
+                if(a[j-1]>a[j]){
                     int temp = a[j];
                     a[j]=a[j+1];
                     a[j+1]=temp;
